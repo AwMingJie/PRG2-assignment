d80388e25e5c759818ac72416e832f5096391343
tmctank <awmj@email.com>
2025-01-15 10:40:43 +0800

    Add project files.

---
+++ S10266864B_PRG2Assignment/Airline.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10266864B_PRG2Assignment
+{
+    class Airline
+    {
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private string code;
+
+		public string Code
+		{
+			get { return code; }
+			set { code = value; }
+		}
+		private Dictionary<string,Flight> flights;
+
+		public Dictionary<string,Flight> Flights
+		{
+			get { return flights; }
+			set { flights = value; }
+		}
+		public Airline(string name, string code, Dictionary<string, Flight> flights)
+		{
+			Name = name;
+			Code = code;
+			Flights = flights;
+		}
+		public bool AddFlight(Flight fly)
+		{
+			return true; //change later
+		}
+		public double CalculateFee()
+		{
+			return 0.0; //change later
+		}
+		public bool RemoveFlight(Flight fly)
+		{
+			return true; //change later
+		}
+		public string ToString()
+		{
+			return "Airline.cs testing";
+		}
+	}
+}
---
+++ S10266864B_PRG2Assignment/Flight.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10266864B_PRG2Assignment
+{
+    abstract class Flight
+    {
+		private string flightNumber;
+
+		public string FlightNumber
+		{
+			get { return flightNumber; }
+			set { flightNumber = value; }
+		}
+		private string origin;
+
+		public string Origin
+		{
+			get { return origin; }
+			set { origin = value; }
+		}
+		private string destination;
+
+		public string Destination
+		{
+			get { return destination; }
+			set { destination = value; }
+		}
+		private DateTime expectedTime;
+
+		public DateTime ExpectedTime
+		{
+			get { return expectedTime; }
+			set { expectedTime = value; }
+		}
+		private string status;
+
+		public string Status
+		{
+			get { return status; }
+			set { status = value; }
+		}
+		public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+		{
+			FlightNumber = flightNumber;
+			Origin = origin;
+			Destination = destination;
+			ExpectedTime = expectedTime;
+			Status = status;
+		}
+		public string ToString()
+		{
+			return $"flight.cs testing";
+		}
+	}
+}
