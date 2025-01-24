972a8299b2a4422f55d87cc785a1ce8ac2e7aea7
May Cherry Aung <mayaungcherry@gmail.com>
2025-01-24 22:36:45 +0800

    Edit program.cs : basic feature 1

--- S10266864B_PRG2Assignment/Program.cs
+++ S10266864B_PRG2Assignment/Program.cs
@@ -24,21 +24,20 @@ void loadfiles_airlines_and_boarding_gates()
             string[] data = s.Split(",");
             Airline airline = new Airline(data[0], data[1]);
             Airlines.Add(data[1], airline);
-            Terminal.AddAirline(airline);
+            terminal.AddAirline(airline);
         }
     }
     using (StreamReader sr = new StreamReader("boardinggates.csv"))
     {
-        string s = sr.ReadLine();
+        string? s = sr.ReadLine();
         while ((s = sr.ReadLine()) != null)
         {
             string[] data = s.Split(",");
             Flight? flight = null;
             BoardingGate boardingGate = new BoardingGate(data[0], Convert.ToBoolean(data[1]), Convert.ToBoolean(data[2]), Convert.ToBoolean(data[3]), flight);
             BoardingGates.Add(data[0], boardingGate);
+            terminal.AddBoardingGate(boardingGate);
         }
-
-
     }
 }
 
